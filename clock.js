02f7fddbc5af0c098a4e30c818ce91e64a2aeea4
Dave Stevens <dave@crowdlab.com>
2014-01-20 21:18:21 +0000

    Use inline svg image.

--- clock.js
+++ clock.js
@@ -4,6 +4,8 @@ var clock_size = 800;
 var view_border_size = 4;
 var view_border_colour = "black";
 
+var face_image = 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="400" width="400"><defs><g id="small"><line x1="200" y1="32" x2="200" y2="34" stroke="black" stroke-width="1"/></g><g id="medium"><line x1="200" y1="32" x2="200" y2="38" stroke="black" stroke-width="1"/></g><g id="large"><line x1="200" y1="32" x2="200" y2="46" stroke="black" stroke-width="2"/></g><g id="ticks"><g transform="rotate(5 200 200)"><use xlink:href="#small"/></g><g transform="rotate(10 200 200)"><use xlink:href="#small"/></g><g transform="rotate(15 200 200)"><use xlink:href="#medium"/></g><g transform="rotate(20 200 200)"><use xlink:href="#small"/></g><g transform="rotate(25 200 200)"><use xlink:href="#small"/></g><g transform="rotate(30 200 200)"><use xlink:href="#large"/></g></g></defs><g transform="rotate(0 200 200)"><use xlink:href="#ticks"/></g><g transform="rotate(30 200 200)"><use xlink:href="#ticks"/></g><g transform="rotate(60 200 200)"><use xlink:href="#ticks"/></g><g transform="rotate(90 200 200)"><use xlink:href="#ticks"/></g><g transform="rotate(120 200 200)"><use xlink:href="#ticks"/></g><g transform="rotate(150 200 200)"><use xlink:href="#ticks"/></g><g transform="rotate(180 200 200)"><use xlink:href="#ticks"/></g><g transform="rotate(210 200 200)"><use xlink:href="#ticks"/></g><g transform="rotate(240 200 200)"><use xlink:href="#ticks"/></g><g transform="rotate(270 200 200)"><use xlink:href="#ticks"/></g><g transform="rotate(300 200 200)"><use xlink:href="#ticks"/></g><g transform="rotate(330 200 200)"><use xlink:href="#ticks"/></g></svg>';
+
 var view = document.createElement("div");
 view.id = "clock";
 view.style.width = view_size + "px";
@@ -14,7 +16,7 @@ view.style.WebkitBorderRadius = "50%";
 view.style.MozBorderRadius = "50%";
 view.style.borderRadius = "50%";
 view.style.border = view_border_size + "px solid " + view_border_colour;
-view.style.background = "url(clock.svg) no-repeat";
+view.style.background = "url('" + face_image + "')";
 view.style.backgroundSize = clock_size + "px " + clock_size + "px";
 
 var hand_size = 2;
